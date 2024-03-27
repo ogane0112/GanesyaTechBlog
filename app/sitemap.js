8f6afaf0e78758cf97079b9e351aa6303066ab9f
ogane0112 <kai513272@gmail.com>
2024-03-27 13:58:12 +0900

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+    //domain変更の時は,ここを変更するのみで良き！
+    const domain = "https://ganesya-tech-blog.vercel.app/"
+    return [
+      {
+        url: `${domain}`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+      {
+        url: `${domain}/blog`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: `${domain}/list`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+    ]
+  }
