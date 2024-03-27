70d3c4249e680379e2af5814349b8d0fab4ee6de
ogane0112 <kai513272@gmail.com>
2024-03-27 12:35:20 +0900

    add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/app/components/Header"
 
 const inter = Inter({ subsets: ["latin"] });
 
+
 export const metadata: Metadata = {
   title: "ガネーシャテックブログ",
   description: "技術に関する事を発信しています！",
@@ -12,6 +13,24 @@ export const metadata: Metadata = {
     google: '_BxN-o63Dnkf6J_nePzoJikU0G-6Fls3VtqtnpIfVQA',
     yandex: 'yandex',
     yahoo: 'yahoo',
+  },
+  openGraph: {
+    title: "ガネーシャテックブログ",
+    description:"技術に関する事を発信しています！",
+    url:"https://ganesya-tech-blog.vercel.app/",
+    siteName:"ガネーシャテックブログ",
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "ガネーシャテックブログ",
+    description:"https://ganesya-tech-blog.vercel.app/",
+    site: '@ganesya0112',
+    creator: '@ganesya0112',
+  },
+  alternates: {
+    canonical: "https://ganesya-tech-blog.vercel.app/",
   },
 
 };
