1391e572b85fb977edc6250c6b877da7b650e5ee
ogane0112 <kai513272@gmail.com>
2024-03-28 09:41:25 +0900

    change google console content

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,25 @@ import "./globals.css";
 import Header from "@/app/components/Header"
 
 const inter = Inter({ subsets: ["latin"] });
+
 const url = "https://www.ganesyatech.com/"
 
 export const metadata: Metadata = {
+
   metadataBase:new URL(url),
+
   title: "ガネーシャテックブログ",
+
   description: "技術に関する事を発信しています！",
+
   verification: {
-    google: '_BxN-o63Dnkf6J_nePzoJikU0G-6Fls3VtqtnpIfVQA',
+    google: 'FhlgtoB7lWygYhMXUaENJ_u9kblf83cDrrL9uG39MKE',
     yandex: 'yandex',
     yahoo: 'yahoo',
   },
+
   openGraph: {
+
     title: "ガネーシャテックブログ",
     images:"/opengraph-image.png",
     description:"技術に関する事を発信しています！",
@@ -23,24 +30,31 @@ export const metadata: Metadata = {
     siteName:"ガネーシャテックブログ",
     locale: 'ja_JP',
     type: 'website',
+
   },
   twitter: {
+
     card: 'summary_large_image',
     title: "ガネーシャテックブログ",
     description:url,
     site: '@ganesya0112',
     creator: '@ganesya0112',
     images:"/opengraph-image.png",
+
   },
   alternates: {
+
     canonical: url,
+
   },
 
 };
 
 
 export default function RootLayout({
+
   children,
+  
 }: Readonly<{
   children: React.ReactNode;
 }>) {
