06d1e3981132f7d084404d06d7f478910f6953e5
ogane0112 <kai513272@gmail.com>
2024-03-29 11:31:54 +0900

    add sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -11,13 +11,13 @@ export default function sitemap() {
         priority: 1,
       },
       {
-        url: `${domain}/blog`,
+        url: `${domain}/blog/[id]`,
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.8,
       },
       {
-        url: `${domain}/list`,
+        url: `${domain}/list/[id]`,
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
